93f41e3e4f4178270f5e4fbc11d9f609a63e1479
BlueGeckoJP <kei09doya@gmail.com>
2026-01-08 18:41:28 +0900

    add lewis6991/gitsigns.nvim plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,4 +11,21 @@ return {
 			{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff" },
 		},
 	},
+
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "|" },
+					change = { text = "|" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+			})
+		end,
+	},
 }
