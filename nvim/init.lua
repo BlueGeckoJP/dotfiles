c3540f48a1e3cddf3bc4fe8fa860ec23875aaed7
BlueGeckoJP <kei09doya@gmail.com>
2026-01-09 09:55:57 +0900

    change tab keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,6 +15,14 @@ return {
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
 
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = unpack(vim.api.nvim_win_getcursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+		end
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -36,7 +44,7 @@ return {
 				["<C-e>"] = cmp.mapping.abort(),
 
 				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
+					if cmp.visible() and has_words_before() then
 						cmp.confirm({ select = true })
 					else
 						fallback()
