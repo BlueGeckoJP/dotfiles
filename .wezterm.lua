9936c0459c64ee247a35bcc8cb383f0033cfda41
BlueGeckoJP <kei09doya@gmail.com>
2026-01-24 21:08:39 +0900

    add tmux and wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 11
+config.font = wezterm.font("UDEV Gothic 35NF")
+config.default_prog = { "tmux", "new-session", "-A", "-s", "main", "zsh" }
+config.color_scheme = "Catppuccin Macchiato (Gogh)"
+
+config.keys = {
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
+}
+
+return config
