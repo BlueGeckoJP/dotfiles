73f380138fa73fe4fa9e20a794392acc081d7d77
BlueGeckoJP <kei09doya@gmail.com>
2026-01-31 06:25:29 +0900

    change wezterm colorscheme to catppuccin latte

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,8 @@ local config = wezterm.config_builder()
 config.font_size = 10
 config.font = wezterm.font("UDEV Gothic 35NF")
 config.default_prog = { "tmux", "new-session", "-A", "-s", "main", "zsh" }
-config.color_scheme = "Catppuccin Macchiato (Gogh)"
+config.color_scheme = "Catppuccin Latte (Gogh)"
+config.enable_tab_bar = false
 
 config.keys = {
 	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
