f24866dffeebbdef7209c70e38007613cb6382da
BlueGeckoJP <kei09doya@gmail.com>
2026-01-30 17:40:32 +0900

    change wezterm's font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font_size = 11
+config.font_size = 10
 config.font = wezterm.font("UDEV Gothic 35NF")
 config.default_prog = { "tmux", "new-session", "-A", "-s", "main", "zsh" }
 config.color_scheme = "Catppuccin Macchiato (Gogh)"
